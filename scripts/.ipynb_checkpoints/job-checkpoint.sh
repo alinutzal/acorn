46b30d17a37b88a45d2da5debe62207e0cc80173
alina <alinutzal@gmail.com>
2024-02-09 14:26:42 -0800

    hard cuts

---
+++ scripts/.ipynb_checkpoints/job-checkpoint.sh
@@ -0,0 +1,1 @@
+sbatch -n 4 ./scripts/submit_train16.sh #acorn train ../examples/Example_1/gnn_train.yaml
---
+++ scripts/.ipynb_checkpoints/submit_train-checkpoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH -A m4439_g
+#SBATCH -C gpu&hbm80g
+#SBATCH -N 1
+#SBATCH -q regular
+#SBATCH -c 32
+#SBATCH --gpu-bind=none
+#SBATCH --requeue 
+#SBATCH -o logs/%x-%j.out
+#SBATCH -J train
+#SBATCH --mail-type=ALL
+#SBATCH --signal=SIGUSR1@90
+#SBATCH --time=01:00:00
+#SBATCH --gpus-per-node=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --module=gpu,nccl-2.18
+
+mkdir -p logs
+
+export SLURM_CPU_BIND="cores"
+export CUDA_LAUNCH_BLOCKING=1
+export FI_MR_CACHE_MONITOR=userfaultfd
+export NCCL_NET_GDR_LEVEL=PHB
+
+# Setup software
+module load python
+conda activate acorn
+
+WANDB__SERVICE_WAIT=300 srun -u acorn train examples/Example_1/gnn_train.yaml
